b110ee929ecdff9e5748f93e51291613cb379cfb
HD <alirezazarabi@gmail.com>
2023-12-14 15:02:25 +0330

    init publisher confirms using sprig cloud stream project.

---
+++ rabbit-mq/publisher-confirms-spring-cloud-stream/src/main/java/com/devalz/toturials/rabbitmq/publisherconfirmsspringcloudstream/PublisherConfirmsSpringCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.devalz.toturials.rabbitmq.publisherconfirmsspringcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherConfirmsSpringCloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherConfirmsSpringCloudStreamApplication.class, args);
+	}
+
+}
---
+++ rabbit-mq/publisher-confirms-spring-cloud-stream/src/test/java/com/devalz/toturials/rabbitmq/publisherconfirmsspringcloudstream/PublisherConfirmsSpringCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalz.toturials.rabbitmq.publisherconfirmsspringcloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherConfirmsSpringCloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
